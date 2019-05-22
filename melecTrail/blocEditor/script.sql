0cee3c49b2d6c2216d2350ea4022e95a894aaed1
BenjaminPh <phelippeaub@gmail.com>
2019-05-22 11:54:56 +0200

    Script SQL
    rajout tables Block et Page

---
+++ melecTrail/blocEditor/script.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS `block`;
+CREATE TABLE IF NOT EXISTS `block` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `pageId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `page`;
+CREATE TABLE IF NOT EXISTS `page` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentId` int(11) NOT NULL,
+  `editable` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
