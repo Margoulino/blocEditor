152c9f616eebb727bf39727740107fdac87bf82c
Margoulino <ludovic.mahe0@gmail.com>
2019-05-22 14:22:54 +0200

    blockModel save Update

--- melecTrail/blocEditor/model/blockModel.php
+++ melecTrail/blocEditor/model/blockModel.php
@@ -9,7 +9,7 @@ class BlockModel
     public $type;
     public $content;
     public $idPage;
-    //public $order;
+    public $order;
 
     public function __construct($data = null)
     {
@@ -35,5 +35,57 @@ class BlockModel
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_CLASS, 'BlockModel');
         return $stmt->fetchAll();
+    }
+
+    /** 
+     * Save function :
+     * Creates and saves a block in the database
+     * 
+     */
+    public static function save(\BlockModel $block)
+    {
+        if (isset($block->id)) {
+            return update($block);
+        }
+        $dbConn = DBModel::getConnection();
+        $stmt = $dbConn->prepare('
+            INSERT INTO block 
+                (name, type, content, idPage, order) 
+            VALUES 
+                (:name, :type, :content, :idPage, :order)
+        ');
+        $stmt->bindParam(':name', $block->name);
+        $stmt->bindParam(':type', $block->type);
+        $stmt->bindParam(':content', $block->content);
+        $stmt->bindParam(':idPage', $block->idPage);
+        $stmt->bindParam(':order', $block->order);
+        return $stmt->execute();
+    }
+
+    /** 
+     * Update function :
+     * Updates and saves a block in the database
+     * 
+     */
+    public function update()
+    {
+        $dbConn = DBModel::getConnection();
+        $stmt = $dbConn->prepare(
+            '
+        UPDATE `block`
+         SET `name`    = :name,
+             `type`    = :type,
+             `content` = :content,
+             `idPage`  = :idPage,
+             `order`   = :order
+         WHERE `id` = :id'
+        );
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':type', $this->type);
+        $stmt->bindParam(':content', $this->content);
+        $stmt->bindParam(':idPage', $this->idPage);
+        $stmt->bindParam(':order', $this->order);
+        $stmt->bindParam(':id', $this->id);
+        return $stmt->execute();
     }
 }
