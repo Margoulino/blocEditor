ea0e64074bef0378ca3b965e126f3c4f2c984ef8
Margoulino <ludovic.mahe0@gmail.com>
2019-05-22 16:59:30 +0200

    blockController save

--- melecTrail/blocEditor/controller/blockController.php
+++ melecTrail/blocEditor/controller/blockController.php
@@ -1,8 +1,9 @@
 <?php
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/blocEditor/model/blockModel.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/blocEditor/model/pageModel.php';
+
 require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
-use \Firebase\JWT\JWT;
 
 class BlockController
 {
@@ -21,6 +22,25 @@ class BlockController
     }
 
     /**
-     * test
+     * Adds a block to an existing page(?)
      */
+    public function addBlockToPage()
+    {
+        $this->setHeader();
+        $data = json_decode(file_get_contents("php://input"));
+        $block = new BlockModel();
+        $block->name = $data->name;
+        $block->type = $data->type;
+        $block->content = $data->content;
+        $block->order = $data->order;
+        $block->idPage = $data->idPage;
+        
+        $targetPage = PageModel::findByid($data->idPage);
+
+        BlockModel::save($block);
+        http_response_code(200);
+        echo json_encode(array("message" => "Block succesfully added to base"));
+    }
+
+
 }
