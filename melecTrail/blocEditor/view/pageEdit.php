6583a105d3f818b4459db20356a4abdd3adf8cb6
Margoulino <ludovic.mahe0@gmail.com>
2019-06-27 10:53:48 +0200

    Probème affichage img picker corrigé

--- melecTrail/blocEditor/view/pageEdit.php
+++ melecTrail/blocEditor/view/pageEdit.php
@@ -130,9 +130,9 @@
                         <div class="col-md-6">
                             <select class="image-picker">
                                 <?php
-                                foreach (scandir('./blocEditor/img') as $file) {
+                                foreach (scandir('./blocEditor/asset/img') as $file) {
                                     if ($file != "." && $file != "..") {
-                                        echo '<option data-img-src="/blocEditor/img/' . $file . '" value="' . $file . '"></option>';
+                                        echo '<option data-img-src="/blocEditor/asset/img/' . $file . '" value="' . $file . '"></option>';
                                     }
                                 }
                                 ?>
@@ -228,7 +228,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/12.1.0/classic/ckeditor.js"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/dropzone.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/image-picker/0.3.1/image-picker.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.0/js/lightbox.min.js"></script>
